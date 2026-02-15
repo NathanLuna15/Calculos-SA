411635e6ec9bd444061504fae275ca1b63f7d28b
NathanLuna15 <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>
2026-02-15 13:38:16 -0300

    virgula adicoionada

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const entradaDeDados = readline.createInterface({
 })
 
 entradaDeDados.question("digite o primeiro numero: ", function(n1){
-    let valor1 = n1
+    let valor1 = Number(n1.replace(',', '.'));
 
     entradaDeDados.question("digite o segumdo numero: ", function(n2){
-        let valor2 = n2
+        let valor2 = Number(n2.replace(',', '.'));
 
         entradaDeDados.question("qual operação matematica você deseja fazer? (multiplicação, adição, subtração ou divisão): ", function(tipo){
             let operacao = tipo 
@@ -20,9 +20,8 @@ entradaDeDados.question("digite o primeiro numero: ", function(n1){
             let retorno = validar.validado(valor1, valor2, operacao)
             let calcullos = calcular.caucular(valor1, valor2, operacao)
 
-    
           console.log(retorno)
-          console.log(calcullos)
+          console.log(calcullos.replace('.', ','))
 
         })
     })                   
